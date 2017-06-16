681d0abf243ddc683c5e7e55620c62b78afe0c38
DESKTOP-TG87KQI\Flee <fleevrl@gmail.com>
2017-06-16 18:36:15 -0300

    Alter fotos-controller.js -> $http and promise using ng-repeat on index.html

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,17 +1,17 @@
-angular.module('fanpic').controller('FotosController', function($scope) {
+angular.module('fanpic').controller('FotosController', function($scope, $http) {
 
-	$scope.fotos = [
-		{
-			titulo: 'Leão 1',
-	        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-		},
-		{
-			titulo: 'Leão 2',
-	        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-		},
-		{
-			titulo: 'Leão 3',
-	        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-		},
-	]	
+	$scope.fotos = [];
+
+	$http.get('v1/fotos').success(function(fotos) {
+		$scope.fotos = fotos;
+	}).error(function(erro) {
+		console.log(erro);
+	});
+
+	// var promise = $http.get('v1/fotos');
+	// promise.then(function(retorno) {
+	// 	$scope.fotos = retorno.data;
+	// }).catch(function(error) {
+	// 	console.log(error);
+	// });
 });
