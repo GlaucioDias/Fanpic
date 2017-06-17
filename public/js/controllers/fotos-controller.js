93c08869a2b95ac10757c6accc0090b741d2275d
Gláucio Dias <fleevrl@gmail.com>
2017-06-17 18:53:30 -0300

    Alter controller

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,16 +2,16 @@ angular.module('fanpic').controller('FotosController', function($scope, $http) {
 
 	$scope.fotos = [];
 
-	$http.get('v1/fotos').success(function(fotos) {
-		$scope.fotos = fotos;
-	}).error(function(erro) {
-		console.log(erro);
-	});
-
-	// var promise = $http.get('v1/fotos');
-	// promise.then(function(retorno) {
-	// 	$scope.fotos = retorno.data;
-	// }).catch(function(error) {
-	// 	console.log(error);
+	// $http.get('v1/fotos').success(function(fotos) {
+	// 	$scope.fotos = fotos;
+	// }).error(function(erro) {
+	// 	console.log(erro);
 	// });
+
+	var promise = $http.get('v1/fotos');
+	promise.then(function(retorno) {
+		$scope.fotos = retorno.data;
+	}).catch(function(error) {
+	 	console.log(error);
+	 });
 });
