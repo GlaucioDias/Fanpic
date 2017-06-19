787e12e1aceb90ad858a7b72958a7ac043d0159e
DESKTOP-TG87KQI\Flee <fleevrl@gmail.com>
2017-06-19 17:05:30 -0300

    Criações: foto-controller.js -> $scope.foto, $scope.submeter foto.html -> form para salvar novas fotos principal.html -> view principal com  chamada para fotos.html Alterações: preparação dos arquivos index.html, main.js e minhas-diretivas.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
-angular.module('fanpic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('fanpic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+	.config(function($routeProvider, $locationProvider) {
+
+		$locationProvider.html5Mode(true); //habilita o locationProvider para retirada do '#' na barra de endereço
+
+		$routeProvider.when('/fotos', { //aponta para o principal.html
+			templateUrl: 'partials/principal.html',
+			controller: 'FotosController'
+		});
+
+		$routeProvider.when('/fotos/new', {
+			templateUrl: 'partials/foto.html',
+			controller: 'FotoController'
+		});
+
+		$routeProvider.otherwise({redirectTo: '/fotos'});
+	});
